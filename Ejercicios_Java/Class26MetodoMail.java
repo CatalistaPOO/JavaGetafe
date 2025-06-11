f05232db9896ce941230f301767befe79823404e
CatalistaPOO <fgtpoo78@gmail.com>
2025-06-11 13:54:55 +0200

    Pendiente

--- Ejercicios_Java/Class26MetodoMail.java
+++ Ejercicios_Java/Class26MetodoMail.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Class26MetodoMail {
     public static void main(String[] args) {
         // Pedir mail y valorar si es correcto o no 
@@ -5,6 +7,35 @@ public class Class26MetodoMail {
         // indicando si es true o false.
         // En main tendremos la petición al usuario del mail
 
-        
+        Scanner teclado = new Scanner(System.in);
+        String mail = teclado.nextLine();
+
+
+
+    }
+
+    public static boolean (String email){
+        boolean validMail= false;
+        if (email.contains("@")==true && email.contains(".")==true && email.length() != 0 
+                || (email.startsWith("@") || email.endsWith("@"))){
+            
+                || (email.indexOf("@") != email.lastIndexOf("@") )
+            else if(email.indexOf(".") > 0){
+                System.out.println("ERROR: no hay dominio ");
+            }
+            else if(email.lastIndexOf("@") > email.lastIndexOf(".")){
+                System.out.println("ERROR: tu dominio (.) empieza antes de tu email (@)");
+            }
+            else if(email.lastIndexOf(".") <= (email.length() - 3)|| email.lastIndexOf(".") <=(email.length() - 2)){
+                System.out.println("ERROR: Dominio demasiado largo");
+            }
+            else{
+                System.out.println("E-mail válido");
+            }
+        }
+        else{
+            System.out.println("Tu mail no tiene @  o  . ");
+        }
+        return istrue;
     }
 }
